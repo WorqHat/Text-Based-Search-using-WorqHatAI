116dbe6e9f0e406d16f5437aac1303ac52294e41
Sagnik Ghosh <sagnik@worqhat.com>
2023-08-06 17:22:47 +0530

    fix: Complete the Demo Process

--- javascript/index.js
+++ javascript/index.js
@@ -82,3 +82,62 @@ window.addEventListener("load", () => {
     createBlocks(movie);
   });
 });
+
+// Search API Integration
+
+// So I have already created my account on WorqHat, created an Organization Workspace and I now have my API Key. I have also trained my search Model and have the Model ID. I have trained it using the Same Movie Data and I have taken the No-Code Approach. You can also train your data using the APIs as well.
+
+// You can use the Training UI, when your data is going to remain constant and you can use the APIs when your data is going to change frequently. You can also add more data to your model using the APIs and the WorqHat UI as well.
+
+// To create your own workspace, visit https://worqhat.com and click on the Get Started Button to Create a Workspace.
+
+/**
+ * The `fetchSearchData` function sends a POST request to an API endpoint with a query and retrieves
+ * search results, which are then displayed on a webpage.
+ * @param query - The `query` parameter is the search query or question that you want to send to the
+ * API for fetching search data. It is the text that represents the user's search input or question.
+ */
+function fetchSearchData(query) {
+  const data = JSON.stringify({
+    question: query,
+    training_data: "3b6c76a4-04c4-45f2-8372-09e2ee34f33e",
+  });
+
+  const xhr = new XMLHttpRequest();
+
+  xhr.addEventListener("readystatechange", function () {
+    if (this.readyState === this.DONE) {
+      var content = JSON.parse(this.responseText);
+      console.log(content);
+      document.getElementById("movieGrid").innerHTML = "";
+      content.result.forEach((movie) => {
+        createBlocks(movie);
+      });
+      document.getElementById("counter").textContent = content.result.length;
+    }
+  });
+
+  xhr.open("POST", "https://api.worqhat.com/api/ai/search/v2");
+  xhr.setRequestHeader(
+    "Authorization",
+    "Bearer sk-721170e3cd914bd08a2f77113815d38e"
+  );
+  xhr.setRequestHeader("Content-Type", "application/json");
+
+  xhr.send(data);
+}
+
+/*
+  First I need to rename by Input Element to searchMovies and the search button to searchButton. I will also add an event listener to the searchButton to listen for the click event. I will then call the fetchSearchData function and pass in the value of the searchMovies input element.
+*/
+
+/* This code is adding an event listener to the search button element with the id "searchButton". When
+the button is clicked, it will execute the callback function. Inside the callback function, it
+retrieves the value of the input element with the id "searchMovies" and assigns it to the variable
+"query". It then logs the value of "query" to the console. Finally, it calls the fetchSearchData
+function and passes in the value of "query" as an argument. */
+document.getElementById("searchButton").addEventListener("click", () => {
+  var query = document.getElementById("searchMovies").value;
+  console.log(query);
+  fetchSearchData(query);
+});
